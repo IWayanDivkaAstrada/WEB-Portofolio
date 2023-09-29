40ca62e6c5dd77847f6c4218d9e6d3490b306aef
IWayanDivkaAstrada <127764669+IWayanDivkaAstrada@users.noreply.github.com>
2023-09-29 17:22:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+if (window.innerWidth < 768) {
+    // Lakukan sesuatu untuk tampilan kecil
+  } else if (window.innerWidth >= 768 && window.innerWidth < 1024) {
+    // Lakukan sesuatu untuk tampilan sedang
+  } else {
+    // Lakukan sesuatu untuk tampilan besar
+  }
